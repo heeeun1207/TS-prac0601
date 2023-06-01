111f24b0ee939dbf96bc04aadc762906658c2355
jungheeeun <jungheeeun272@gmail.com>
2023-06-01 09:58:24 +0900

    사칙연산

---
+++ main.ts
@@ -0,0 +1,6 @@
+import { add, subtract, multiply, divide } from './operations';
+
+console.log(add(5, 3));      //  8
+console.log(subtract(5, 3)); //  2
+console.log(multiply(5, 3)); //  15
+console.log(divide(6, 2));   //  3
